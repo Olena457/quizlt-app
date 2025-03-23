7af184ceb73ff4b62779c35c0feddd03656dad67
Olena457 <olenademianenko42@gmail.com>
2025-03-23 16:24:41 +0200

    added common folders

---
+++ src/components/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,7 @@
+import css from './LoginPage.module.css';
+
+const LoginPage = () => {
+  return <div className={css.container}></div>;
+};
+
+export default LoginPage;
---
+++ src/components/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,7 @@
+import css from './RegistrationPage.module.css';
+
+const RegistrationPage = () => {
+  return <div className={css.container}></div>;
+};
+
+export default RegistrationPage;
