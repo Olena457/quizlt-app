f712605e738a14be0d0fbd8106271de43ff07cdd
Olena457 <olenademianenko42@gmail.com>
2025-03-24 04:25:41 +0200

    aded navigaton and icons

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,55 @@
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectorsAuth.js';
 import css from './Navigation.module.css';
 
 const Navigation = () => {
-  return <div className={css.nav}></div>;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const activeLink = ({ isActive }) => clsx(css.link, isActive && css.active);
+
+  return (
+    <div className={css.container}>
+      <NavLink to="/" className={activeLink} aria-label="Home">
+        Home
+      </NavLink>
+      <NavLink to="/cards" className={activeLink} aria-label="cards">
+        Events
+      </NavLink>
+      {!isLoggedIn && (
+        <>
+          <NavLink to="/login" className={activeLink} aria-label="Login">
+            Login
+          </NavLink>
+          <NavLink
+            to="/register-user"
+            className={activeLink}
+            aria-label="Register"
+          >
+            Register
+          </NavLink>
+        </>
+      )}
+      {isLoggedIn && (
+        <>
+          <NavLink
+            to="/favorites"
+            className={activeLink}
+            aria-label="Favorites"
+          >
+            Favorites
+          </NavLink>
+          <NavLink
+            to="/create-card"
+            className={activeLink}
+            aria-label="Create card"
+          >
+            Create Card
+          </NavLink>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Navigation;
